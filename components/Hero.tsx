6dc30e307a7dafa2741397b35c1b48851972131c
edberaga <edbertjonnathan@gmail.com>
2025-05-29 13:39:12 +0700

    Improve data input for the homepage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,6 +3,8 @@ import banner from '../public/banner.png'
 import { motion } from 'framer-motion';
 import { Monitor, BarChart2, Wallet, FileText } from 'lucide-react';
 import Image from 'next/image';
+import data from './data/Hero.json'
+import Link from 'next/link';
 
 export default function Hero() {
   const services = [
@@ -32,29 +34,33 @@ export default function Hero() {
         ))}
       </div>
       
-      <div className="container mx-auto px-4 pt-32 pb-20">
+      <div className="container mx-auto px-4 pt-36 pb-10">
         <div className="grid lg:grid-cols-2 gap-12 items-center">
           <motion.div
             initial={{ opacity: 0, x: -50 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.8 }}
           >
-            <h1 className="text-5xl lg:text-6xl font-bold text-gray-900 mb-6">
-              Find the best<br />
-              solution together.
-            </h1>
+            <h6 className='primary uppercase font-bold text-gray-900 mb-3'>
+              {data.SubTitle}
+            </h6>
+            <h2 className="text-2xl lg:text-4xl font-bold text-gray-900 mb-6">
+              {data.Title}
+            </h2>
             <p className="text-lg text-gray-600 mb-8">
-              We not only provide consultation but also consider your thoughts to find the best solution for each of your business problems.
+              {data.Desc}
             </p>
             <div className="flex gap-4 mb-12">
-              <motion.button
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ delay: 0.2 }}
-                className="px-6 py-3 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition-colors"
-              >
-                Get Started
-              </motion.button>
+              <Link href="#about">
+                <motion.button
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 0.2 }}
+                  className="px-6 py-3 btn-primary shadow-indigo-500/50 rounded-lg hover:bg-purple-600 transition-colors"
+                >
+                  Get Started
+                </motion.button>
+              </Link>
               <motion.button
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
